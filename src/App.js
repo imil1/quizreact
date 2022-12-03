32588b4db561931b94a37b2fc851c6501778f5b3
imil <imil.net@mail.ru>
2022-12-03 16:39:55 +0500

    quizreact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React from 'react';
 import './index.scss';
 
 const questions = [
   {
+    title: 'Что такое useState ?',
+    variants: ['Это функция для хранения данных компонента', 'Это глобальный стейт', 'Это когда ты никому не нужен'],
+    correct: 0,
+  }, 
+  {
+    title: 'for(var x=1;x<5;x++) console.log(x)',
+    variants: ['11111', '5555', '1234'],
+    correct: 2,
+  },
+  {
+    title: 'x=1; console.log(`x= `+ x); var x;',
+    variants: ['x=1', 'x is undefined', 'x=null'],
+    correct: 0,
+  },
+   {
+    title: 'let a = `3`+ 2-1;',
+    variants: ['32', 'error', '31'],
+    correct: 2,
+  },
+  {
     title: 'React - это ... ?',
     variants: ['библиотека', 'фреймворк', 'приложение'],
     correct: 0,
-  },
+  },  
   {
     title: 'Компонент - это ... ',
     variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
@@ -22,37 +43,56 @@ const questions = [
   },
 ];
 
-function Result() {
+function Result({correct}) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
+      <a href="/">
+      <button >Попробовать снова</button>
+      </a>
     </div>
   );
 }
 
-function Game() {
+function Game({step,question,onClickVar}) {
+  const perc = Math.round(step / questions.length * 100);
+  console.log(perc);
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${perc}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+       {question.variants.map((text,index)=> (
+        <li onClick={()=> onClickVar(index)} key={text}>{text}</li>
+        ))}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step,setStep] = React.useState(0);
+  const [correct,setCorrect] = React.useState(0);
+  const question = questions[step];
+  // console.log(question);
+  const onClickVar = (index) => {
+    console.log(step,index);
+    setStep(step+1);
+    if(index === question.correct){
+      setCorrect(correct+1);
+    }
+  };
+
+
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {
+        step !== questions.length ? <Game step={step} question={question} onClickVar={onClickVar}/> : <Result correct={correct}/> 
+      }
+      
     </div>
   );
 }
